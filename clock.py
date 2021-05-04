1baa8e0023a455d6d1b40159ad85711cb10934f2
Rohan Krishna Ullas <71774184+Kakarot-2000@users.noreply.github.com>
2021-05-04 14:59:49 +0530

    Update clock.py

--- clock.py
+++ clock.py
@@ -1,16 +1,16 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
-from run import fun1
+from run import sendMail
 sched = BlockingScheduler()
 
 '''
 @sched.scheduled_job('interval', minutes=1)
 def timed_job():
     print('This job is run every one minute.')
-    fun1()
+    sendMail()
 '''
 
 @sched.scheduled_job('cron', day_of_week='mon-fri', hour=8)
 def scheduled_job():
     print('This job is run every weekday at 8 AM.')
-    fun1()
+    sendMail()
 sched.start()
